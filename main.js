3861c0f380292d97ffcbea4c9345a362d72d2262
Abdurahim102 <abundance0121@gmail.com>
2024-03-25 17:57:53 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+let meva = ["shaftoli","olma","Orik","Banan","Piyoz"]
+
+
+// console.log(meva.splice(1,3));
+
+// console.log(meva.splice(1,4));
+// console.log(meva.splice(3,4));
+// console.log(meva.splice(1,2));
+// console.log(meva.splice(2,3));
+
+
+
+
+
+// console.log(meva.slice(0,3)); 
+// console.log(meva.slice(2,3)); 
+// console.log(meva.slice(3,4)); 
+// console.log(meva.slice(1,3)); 
+// console.log(meva.slice(2,4)); 
+
+
+// console.log(meva.find(item => item == "Piyoz"));
+// console.log(meva.find(item => item == "Banan"));
+// console.log(meva.find(item => item == "shaftoli"));
+
+// console.log(meva.findIndex(item => item == "shaftoli")); 
+// console.log(meva.findIndex(item => item == "Banan")); 
+// console.log(meva.findIndex(item => item == "Orik")); 
+
+
+// console.log(meva.filter(item => item == "Piyoz"));
+// console.log(meva.filter(item => item == "olma"));
+// console.log(meva.filter(item => item == "Orik"));
